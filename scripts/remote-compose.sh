48cf7ce148893ef99efda63ae2fd8e12ab3bafcb
it-sla <it@gohimalaya.travel>
2026-09-20 14:34:24 +0545

    Fix deploy: clear dist contents not the mount point

--- scripts/remote-compose.sh
+++ scripts/remote-compose.sh
@@ -91,7 +91,7 @@ case "$command" in
     printf '==> Deploying to %s:%s\n' "$REMOTE_HOST" "$REMOTE_DIR"
     remote git fetch origin
     remote git reset --hard origin/main
-    ssh "$REMOTE_HOST" "cd $REMOTE_DIR && docker run --rm -v \"\$(pwd)/frontend/dist:/target\" alpine sh -c 'rm -rf /target && mkdir /target'"
+    ssh "$REMOTE_HOST" "cd $REMOTE_DIR && docker run --rm -v \"\$(pwd)/frontend/dist:/target\" alpine sh -c 'rm -rf /target/*'"
     remote sh -c "cd frontend && npm ci --prefer-offline && npm run build"
     compose up -d --build
     compose exec -T backend alembic upgrade head
